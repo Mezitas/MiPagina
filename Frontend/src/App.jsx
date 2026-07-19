243e45a5bfbccbbca6e12838febaf324ae14ecf3
mezita <diegopaz04717@gmail.com>
2026-07-19 11:49:49 -0500

    empezamos de nuevo

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hola, soy [Tu Nombre]</h1>
+      <p>Bienvenido a mi portafolio personal. Pronto aquí verán mis proyectos.</p>
+    </div>
+  )
+}
+
+export default App
